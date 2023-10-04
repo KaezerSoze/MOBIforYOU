43202c4a003a98851fa581338fb581dda513ab72
kaezersoze <fingerzjbm@gmail.com>
2023-10-04 10:01:28 +0200

    colab

--- Backend/script.sql
+++ Backend/script.sql
@@ -6,8 +6,16 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE IF NOT EXISTS users (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100),
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS user_comments (
+  id INT NOT NULL AUTO_INCREMENT UNIQUE,
+  user_id INT NOT NULL,
+  comment_content VARCHAR(400) NOT NULL,
+  comment_date TIMESTAMP NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
--- Frontend/src/components/navigation/Router.jsx
+++ Frontend/src/components/navigation/Router.jsx
@@ -1,19 +1,24 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import RegistrationForm from "../authentification/Inscription";
 import LoginForm from "../authentification/Login";
+import Home from "../pages/home"; 
+
 
 export default function Router({ isLoggedIn, setIsLoggedIn }) {
   return (
     <Routes>
-      {/* Redirige vers /auth si l'utilisateur est déjà connecté */}
+      {/* Redirige vers /home si l'utilisateur est déjà connecté */}
       <Route
         path="/"
-        element={isLoggedIn ? <Navigate to="/auth" /> : <RegistrationForm />}
+        element={isLoggedIn ? <Navigate to="/home" /> : <RegistrationForm />}
       />
+      {/* Redirige vers /home si l'utilisateur est déjà connecté */}
       <Route
         path="/login"
-        element={<LoginForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
+        element={isLoggedIn ? <Navigate to="/home" /> : <LoginForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
       />
+      {/* Définissez la route /home ici */}
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
